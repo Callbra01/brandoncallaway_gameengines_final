abe4849e4b281f11e59bdb1b5076ebecde3d210f
wightkid <103604852+wightkid@users.noreply.github.com>
2025-04-07 11:29:07 -0400

    Update startscreen Prepare for test assets

--- Assets/Scripts/StartScript.cs
+++ Assets/Scripts/StartScript.cs
@@ -2,21 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartScript : MonoBehaviour
 {
+    public TextMeshProUGUI startText;
+    public TextMeshProUGUI exitText;
+    public float selectedTextSize;
+    public float deselectedTextSize;
 
-    TextMeshPro startText;
-    TextMeshPro exitText;
+    int currentSelectionIndex;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentSelectionIndex = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleInput();
+        HandleSelection();
+    }
+
+    // Update both start and exit text fontSize based on selection index
+    void HandleSelection()
+    {
+        if (currentSelectionIndex == 0)
+        {
+            startText.fontSize = selectedTextSize;
+            exitText.fontSize = deselectedTextSize;
+        }
+
+        if (currentSelectionIndex == 1)
+        {
+            startText.fontSize = deselectedTextSize;
+            exitText.fontSize = selectedTextSize;
+        }
+    }
+
+    // Set current selection index, based on user input
+    // Quit game / Change scene upon spacebar press
+    void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (currentSelectionIndex > 0)
+            {
+                currentSelectionIndex--;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (currentSelectionIndex < 1)
+            {
+                currentSelectionIndex++;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (currentSelectionIndex == 0)
+            {
+                SceneManager.LoadScene(3);
+            }
+
+            if (currentSelectionIndex == 1)
+            {
+                Application.Quit();
+            }
+        }
     }
 }
